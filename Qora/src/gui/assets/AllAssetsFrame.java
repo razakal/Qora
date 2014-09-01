58892dc573633b4396e89eacddf1f0c6b654ddf9
qora <admin@qora.org>
2014-09-01 21:29:45 +0200

    Forgot to add new files :(

---
+++ Qora/src/gui/assets/AllAssetsFrame.java
@@ -0,0 +1,175 @@
+package gui.assets;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import gui.QoraRowSorter;
+import gui.models.AssetsTableModel;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import qora.assets.Asset;
+
+@SuppressWarnings("serial")
+public class AllAssetsFrame extends JFrame{
+	
+	private AssetsTableModel assetsTableModel;
+
+	public AllAssetsFrame() {
+		
+		super("Qora - Asset Details");
+		
+		//ICON
+		List<Image> icons = new ArrayList<Image>();
+		icons.add(Toolkit.getDefaultToolkit().getImage("images/icons/icon16.png"));
+		icons.add(Toolkit.getDefaultToolkit().getImage("images/icons/icon32.png"));
+		icons.add(Toolkit.getDefaultToolkit().getImage("images/icons/icon64.png"));
+		icons.add(Toolkit.getDefaultToolkit().getImage("images/icons/icon128.png"));
+		this.setIconImages(icons);
+		
+		//CLOSE
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		//LAYOUT
+		this.setLayout(new GridBagLayout());
+		
+		//PADDING
+		((JComponent) this.getContentPane()).setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		//SEACH LABEL GBC
+		GridBagConstraints searchLabelGBC = new GridBagConstraints();
+		searchLabelGBC.insets = new Insets(0, 5, 5, 0);
+		searchLabelGBC.fill = GridBagConstraints.HORIZONTAL;   
+		searchLabelGBC.anchor = GridBagConstraints.NORTHWEST;
+		searchLabelGBC.weightx = 0;	
+		searchLabelGBC.gridwidth = 1;
+		searchLabelGBC.gridx = 0;
+		searchLabelGBC.gridy = 0;
+		
+		//SEACH GBC
+		GridBagConstraints searchGBC = new GridBagConstraints();
+		searchGBC.insets = new Insets(0, 5, 5, 0);
+		searchGBC.fill = GridBagConstraints.HORIZONTAL;   
+		searchGBC.anchor = GridBagConstraints.NORTHWEST;
+		searchGBC.weightx = 1;	
+		searchGBC.gridwidth = 1;
+		searchGBC.gridx = 1;
+		searchGBC.gridy = 0;
+		
+		//TABLE GBC
+		GridBagConstraints tableGBC = new GridBagConstraints();
+		tableGBC.insets = new Insets(0, 5, 5, 0);
+		tableGBC.fill = GridBagConstraints.BOTH;  
+		tableGBC.anchor = GridBagConstraints.NORTHWEST;
+		tableGBC.weightx = 1;	
+		tableGBC.weighty = 1;	
+		tableGBC.gridwidth = 2;
+		tableGBC.gridx = 0;	
+		tableGBC.gridy = 1;	
+		
+		//CREATE TABLE
+		this.assetsTableModel = new AssetsTableModel();
+		final JTable assetsTable = new JTable(this.assetsTableModel);
+		
+		//ASSETS SORTER
+		Map<Integer, Integer> indexes = new TreeMap<Integer, Integer>();
+		QoraRowSorter sorter = new QoraRowSorter(this.assetsTableModel, indexes);
+		assetsTable.setRowSorter(sorter);
+		
+		//CREATE SEARCH FIELD
+		final JTextField txtSearch = new JTextField();
+
+		// UPDATE FILTER ON TEXT CHANGE
+		txtSearch.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				onChange();
+			}
+
+			public void removeUpdate(DocumentEvent e) {
+				onChange();
+			}
+
+			public void insertUpdate(DocumentEvent e) {
+				onChange();
+			}
+
+			public void onChange() {
+
+				// GET VALUE
+				String search = txtSearch.getText();
+
+			 	// SET FILTER
+				assetsTableModel.getSortableList().setFilter(search);
+				assetsTableModel.fireTableDataChanged();
+			}
+		});
+
+		// MENU
+		JPopupMenu nameSalesMenu = new JPopupMenu();
+		JMenuItem details = new JMenuItem("Details");
+		details.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = assetsTable.getSelectedRow();
+				row = assetsTable.convertRowIndexToModel(row);
+
+				Asset asset = assetsTableModel.getAsset(row);
+				new AssetDetailsFrame(asset);
+			}
+		});
+		nameSalesMenu.add(details);
+
+		assetsTable.setComponentPopupMenu(nameSalesMenu);
+		assetsTable.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Point p = e.getPoint();
+				int row = assetsTable.rowAtPoint(p);
+				assetsTable.setRowSelectionInterval(row, row);
+				
+				if(e.getClickCount() == 2)
+				{
+					row = assetsTable.convertRowIndexToModel(row);
+					Asset asset = assetsTableModel.getAsset(row);
+					new AssetDetailsFrame(asset);
+				}
+			}
+		});
+
+		this.add(new JLabel("search:"), searchLabelGBC);
+		this.add(txtSearch, searchGBC);
+		this.add(new JScrollPane(assetsTable), tableGBC);
+		
+		//PACK
+		this.pack();
+		//this.setSize(500, this.getHeight());
+		this.setResizable(true);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+
+	public void removeObservers() {
+		this.assetsTableModel.removeObservers();
+	}
+}
---
+++ Qora/src/gui/models/AssetsTableModel.java
@@ -0,0 +1,138 @@
+package gui.models;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import controller.Controller;
+import qora.assets.Asset;
+import utils.ObserverMessage;
+import database.SortableList;
+
+@SuppressWarnings("serial")
+public class AssetsTableModel extends QoraTableModel<Long, Asset> implements Observer
+{
+	public static final int COLUMN_KEY = 0;
+	public static final int COLUMN_NAME = 1;
+	public static final int COLUMN_ADDRESS = 2;
+	public static final int COLUMN_AMOUNT = 3;
+	public static final int COLUMN_DIVISIBLE = 4;
+
+	private SortableList<Long, Asset> assets;
+	
+	private String[] columnNames = {"Key", "Name", "Owner", "Quantity", "Divisible"};
+	
+	public AssetsTableModel()
+	{
+		Controller.getInstance().addObserver(this);
+	}
+	
+	@Override
+	public SortableList<Long, Asset> getSortableList() 
+	{
+		return this.assets;
+	}
+	
+	public Asset getAsset(int row)
+	{
+		return this.assets.get(row).getB();
+	}
+	
+	@Override
+	public int getColumnCount() 
+	{
+		return this.columnNames.length;
+	}
+	
+	@Override
+	public String getColumnName(int index) 
+	{
+		return this.columnNames[index];
+	}
+
+	@Override
+	public int getRowCount() 
+	{
+		return this.assets.size();
+		
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) 
+	{
+		if(this.assets == null || row > this.assets.size() - 1 )
+		{
+			return null;
+		}
+		
+		Asset asset = this.assets.get(row).getB();
+		
+		switch(column)
+		{
+		case COLUMN_KEY:
+			
+			return asset.getKey();
+		
+		case COLUMN_NAME:
+			
+			return asset.getName();
+		
+		case COLUMN_ADDRESS:
+			
+			return asset.getOwner().getAddress();
+			
+		case COLUMN_AMOUNT:
+			
+			return asset.getQuantity();
+			
+		case COLUMN_DIVISIBLE:
+			
+			return asset.isDivisible();
+			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void update(Observable o, Object arg) 
+	{	
+		try
+		{
+			this.syncUpdate(o, arg);
+		}
+		catch(Exception e)
+		{
+			//GUI ERROR
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public synchronized void syncUpdate(Observable o, Object arg)
+	{
+		ObserverMessage message = (ObserverMessage) arg;
+		
+		//CHECK IF NEW LIST
+		if(message.getType() == ObserverMessage.LIST_ASSET_TYPE)
+		{			
+			if(this.assets == null)
+			{
+				this.assets = (SortableList<Long, Asset>) message.getValue();
+				this.assets.registerObserver();
+			}	
+			
+			this.fireTableDataChanged();
+		}
+		
+		//CHECK IF LIST UPDATED
+		if(message.getType() == ObserverMessage.ADD_ASSET_TYPE || message.getType() == ObserverMessage.REMOVE_ASSET_TYPE)
+		{
+			this.fireTableDataChanged();
+		}
+	}
+	
+	public void removeObservers() 
+	{
+		this.assets.removeObserver();
+		Controller.getInstance().deleteObserver(this);
+	}
+}
