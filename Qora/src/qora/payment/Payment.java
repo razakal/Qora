377bea453f1c3c87c70d7761a854ebf6306eea64
Skerberus <SubZero2000@gmx.de>
2015-04-20 19:07:13 +0200

    Fixed bad json value found by agran.

--- Qora/src/qora/payment/Payment.java
+++ Qora/src/qora/payment/Payment.java
@@ -80,7 +80,7 @@ public class Payment {
 		JSONObject payment = new JSONObject();
 								
 		//ADD RECIPIENT/ASSET/AMOUNT
-		payment.put("recipient", this.recipient);
+		payment.put("recipient", this.recipient.toString());
 		payment.put("asset", this.asset);
 		payment.put("amount", this.amount.toPlainString());
 								
