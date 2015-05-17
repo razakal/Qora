564097b278c2ea4384542d6c5b3a9cee4eef9b13
agran <agran@agran.net>
2015-05-17 15:08:46 +0300

    added cache for demanding determine the build date.

--- Qora/src/utils/BuildTime.java
+++ Qora/src/utils/BuildTime.java
@@ -12,48 +12,67 @@ import java.util.zip.ZipEntry;
 
 public class BuildTime
 {
+	private static String bufgetBuildTime = "";
+	private static String bufgetBuildDate = "";
+	private static String bufgetBuildDateTime = "";
+	private static Date d = null;
+	
 	public static String getBuildTime(){
-		SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss");
-		f.setTimeZone(TimeZone.getTimeZone("UTC"));
-		return(f.format(getClassBuildTime()));
+		if(bufgetBuildTime.equals(""))
+		{
+			SimpleDateFormat f = new SimpleDateFormat("HH:mm:ss");
+			f.setTimeZone(TimeZone.getTimeZone("UTC"));
+			bufgetBuildTime = f.format(getClassBuildTime());
+		}
+		return(bufgetBuildTime);	
 	}
 	
 	public static String getBuildDate(){
-		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
-		f.setTimeZone(TimeZone.getTimeZone("UTC"));
-		return(f.format(getClassBuildTime()));
+		if(bufgetBuildDate.equals(""))
+		{
+			SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+			f.setTimeZone(TimeZone.getTimeZone("UTC"));
+			bufgetBuildDate = f.format(getClassBuildTime());
+		}
+		return bufgetBuildDate;
 	}
 	
 	public static String getBuildDateTime(){
-		SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		f.setTimeZone(TimeZone.getTimeZone("UTC"));
-		return(f.format(getClassBuildTime()));
+		if(bufgetBuildDateTime.equals(""))
+		{
+			SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			f.setTimeZone(TimeZone.getTimeZone("UTC"));
+			bufgetBuildDateTime = f.format(getClassBuildTime()); 
+		}
+		return bufgetBuildDateTime;
 	}
 	
 	private static Date getClassBuildTime() {
-	    Date d = null;
-	    Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
-	    URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
-	    if (resource != null) {
-	        if (resource.getProtocol().equals("file")) {
-	            try {
-	                d = new Date(new File(resource.toURI()).lastModified());
-	            } catch (URISyntaxException ignored) { }
-	        } else if (resource.getProtocol().equals("jar")) {
-	            String path = resource.getPath();
-	            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );    
-	        } else if (resource.getProtocol().equals("zip")) {
-	            String path = resource.getPath();
-	            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
-	            //long jfodLastModifiedLong = jarFileOnDisk.lastModified ();
-	            //Date jfodLasModifiedDate = new Date(jfodLastModifiedLong);
-	            try(JarFile jf = new JarFile (jarFileOnDisk)) {
-	                ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));//Skip the ! and the /
-	                long zeTimeLong = ze.getTime ();
-	                Date zeTimeDate = new Date(zeTimeLong);
-	                d = zeTimeDate;
-	            } catch (IOException|RuntimeException ignored) { }
-	        }
+	    if(d == null)
+	    {
+	    	Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
+	    	URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
+	    	if (resource != null) {
+	    		if (resource.getProtocol().equals("file")) {
+	    			try {
+	    				d = new Date(new File(resource.toURI()).lastModified());
+	    			} catch (URISyntaxException ignored) { }
+		        } else if (resource.getProtocol().equals("jar")) {
+		            String path = resource.getPath();
+		            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );    
+		        } else if (resource.getProtocol().equals("zip")) {
+		            String path = resource.getPath();
+		            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
+		            //long jfodLastModifiedLong = jarFileOnDisk.lastModified ();
+		            //Date jfodLasModifiedDate = new Date(jfodLastModifiedLong);
+		            try(JarFile jf = new JarFile (jarFileOnDisk)) {
+		                ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));//Skip the ! and the /
+		                long zeTimeLong = ze.getTime ();
+		                Date zeTimeDate = new Date(zeTimeLong);
+		                d = zeTimeDate;
+		            } catch (IOException|RuntimeException ignored) { }
+		        }
+	    	}
 	    }
 	    return d;
 	}
