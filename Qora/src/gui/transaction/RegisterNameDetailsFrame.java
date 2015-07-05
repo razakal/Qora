1bc72fc3f3d76706db7116e6f8653e7ea4f2bbb5
agran <agran@agran.net>
2015-07-05 10:06:38 +0300

    Added support displaying name value compression in the transaction window DetailsFrame

--- Qora/src/gui/transaction/RegisterNameDetailsFrame.java
+++ Qora/src/gui/transaction/RegisterNameDetailsFrame.java
@@ -15,6 +15,7 @@ import javax.swing.border.EmptyBorder;
 
 import qora.crypto.Base58;
 import qora.transaction.RegisterNameTransaction;
+import utils.GZIP;
 
 @SuppressWarnings("serial")
 public class RegisterNameDetailsFrame extends JFrame
@@ -141,30 +142,48 @@ public class RegisterNameDetailsFrame extends JFrame
 				
 		//VALUE
 		detailGBC.gridy = 7;
-		JTextArea txtAreaValue = new JTextArea(nameRegistration.getName().getValue());
-      	txtAreaValue.setRows(4);
-      	txtAreaValue.setBorder(name.getBorder());
-      	txtAreaValue.setEditable(false);
-		this.add(txtAreaValue, detailGBC);		
+
+		JTextArea txtareaValue = new JTextArea(GZIP.webDecompress(nameRegistration.getName().getValue()));
+		txtareaValue.setRows(10);
+      	txtareaValue.setColumns(43);
+      	txtareaValue.setEditable(false);
+      	
+      	JScrollPane valueScroll = new JScrollPane(txtareaValue);
+      	valueScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+      	valueScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+      	this.add(valueScroll, detailGBC);
+      	      	
+      	//LABEL COMPRESSED
+      	labelGBC.gridy = 8;
+      	JLabel compressedLabel = new JLabel("Compressed:");
+      	this.add(compressedLabel, labelGBC);
+      		
+  		//COMPRESSED
+  		detailGBC.gridy = 8;
+  		final JCheckBox compressed = new JCheckBox();
+  		compressed.setSelected(nameRegistration.getName().getValue().startsWith("?gz!"));
+  		compressed.setEnabled(false);
+      	
+  		this.add(compressed, detailGBC);		
 		
 		//LABEL FEE
-		labelGBC.gridy = 8;
+		labelGBC.gridy = 9;
 		JLabel feeLabel = new JLabel("Fee:");
 		this.add(feeLabel, labelGBC);
 						
 		//FEE
-		detailGBC.gridy = 8;
+		detailGBC.gridy = 9;
 		JTextField fee = new JTextField(nameRegistration.getFee().toPlainString());
 		fee.setEditable(false);
 		this.add(fee, detailGBC);	
 		
 		//LABEL CONFIRMATIONS
-		labelGBC.gridy = 9;
+		labelGBC.gridy = 10;
 		JLabel confirmationsLabel = new JLabel("Confirmations:");
 		this.add(confirmationsLabel, labelGBC);
 								
 		//CONFIRMATIONS
-		detailGBC.gridy = 9;
+		detailGBC.gridy = 10;
 		JLabel confirmations = new JLabel(String.valueOf(nameRegistration.getConfirmations()));
 		this.add(confirmations, detailGBC);	
 		           
--- Qora/src/gui/transaction/UpdateNameDetailsFrame.java
+++ Qora/src/gui/transaction/UpdateNameDetailsFrame.java
@@ -15,6 +15,7 @@ import javax.swing.border.EmptyBorder;
 
 import qora.crypto.Base58;
 import qora.transaction.UpdateNameTransaction;
+import utils.GZIP;
 
 @SuppressWarnings("serial")
 public class UpdateNameDetailsFrame extends JFrame
@@ -141,30 +142,48 @@ public class UpdateNameDetailsFrame extends JFrame
 				
 		//VALUE
 		detailGBC.gridy = 7;
-		JTextArea txtAreaValue = new JTextArea(nameUpdate.getName().getValue());
-      	txtAreaValue.setRows(4);
-      	txtAreaValue.setBorder(name.getBorder());
-      	txtAreaValue.setEditable(false);
-		this.add(txtAreaValue, detailGBC);		
-		
+
+		JTextArea txtareaValue = new JTextArea(GZIP.webDecompress(nameUpdate.getName().getValue()));
+		txtareaValue.setRows(10);
+      	txtareaValue.setColumns(43);
+      	txtareaValue.setEditable(false);
+      	
+      	JScrollPane valueScroll = new JScrollPane(txtareaValue);
+      	valueScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+      	valueScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+      	this.add(valueScroll, detailGBC);
+      	      	
+      	//LABEL COMPRESSED
+      	labelGBC.gridy = 8;
+      	JLabel compressedLabel = new JLabel("Compressed:");
+      	this.add(compressedLabel, labelGBC);
+      		
+  		//COMPRESSED
+  		detailGBC.gridy = 8;
+  		final JCheckBox compressed = new JCheckBox();
+  		compressed.setSelected(nameUpdate.getName().getValue().startsWith("?gz!"));
+  		compressed.setEnabled(false);
+      	
+  		this.add(compressed, detailGBC);		
+      	
 		//LABEL FEE
-		labelGBC.gridy = 8;
+		labelGBC.gridy = 9;
 		JLabel feeLabel = new JLabel("Fee:");
 		this.add(feeLabel, labelGBC);
 						
 		//FEE
-		detailGBC.gridy = 8;
+		detailGBC.gridy = 9;
 		JTextField fee = new JTextField(nameUpdate.getFee().toPlainString());
 		fee.setEditable(false);
 		this.add(fee, detailGBC);	
 		
 		//LABEL CONFIRMATIONS
-		labelGBC.gridy = 9;
+		labelGBC.gridy = 10;
 		JLabel confirmationsLabel = new JLabel("Confirmations:");
 		this.add(confirmationsLabel, labelGBC);
 								
 		//CONFIRMATIONS
-		detailGBC.gridy = 9;
+		detailGBC.gridy = 10;
 		JLabel confirmations = new JLabel(String.valueOf(nameUpdate.getConfirmations()));
 		this.add(confirmations, detailGBC);	
 		           
