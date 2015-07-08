4a46351cf7d703fce7d1b0d03f694dcb8e774ca3
agran <agran@agran.net>
2015-07-08 04:55:44 +0300

    Fix TransactionTimestampComparator. Old give me error in in some cases.

--- Qora/src/utils/TransactionTimestampComparator.java
+++ Qora/src/utils/TransactionTimestampComparator.java
@@ -9,6 +9,11 @@ public class TransactionTimestampComparator implements Comparator<Transaction> {
 	@Override
 	public int compare(Transaction one, Transaction two) 
 	{
-		return (int) (one.getTimestamp() - two.getTimestamp());
+		if(one.getTimestamp() < two.getTimestamp())
+			return -1;
+		else if(one.getTimestamp() > two.getTimestamp()) 
+			return 1;
+		else
+			return 0;
 	}
 }
