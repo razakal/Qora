e7e9a5b462a3868db5e814938b15be8a94bc1a21
Skerberus <Buddelb@googlemail.com>
2015-07-10 22:25:37 +0200

    no wallet fix reverted and formatted NameUtils

--- Qora/src/qora/web/BlogBlackWhiteList.java
+++ Qora/src/qora/web/BlogBlackWhiteList.java
@@ -158,14 +158,8 @@ public class BlogBlackWhiteList {
 		List<Account> resultingAccounts = new CopyOnWriteArrayList<Account>();
 		
 		List<Account> myaccounts;
-		if(Controller.getInstance().doesWalletExists())
-		{
 			myaccounts = new ArrayList<Account>(Controller
 					.getInstance().getAccounts());
-		}else
-		{
-			myaccounts = new ArrayList<Account>();
-		}
 		List<Name> myNames = new ArrayList<Name>(Controller
 				.getInstance().getNamesAsList());
 		
--- Qora/src/utils/NameUtils.java
+++ Qora/src/utils/NameUtils.java
@@ -20,182 +20,158 @@ import database.NameMap;
 
 public class NameUtils {
 
-	public enum NameResult
-	{
-		OK("OK", "OK", 0), 
-		NAME_NOT_REGISTERED("The name is not registered", 
-				"Invalid address or name not registered!", 
-				ApiErrorFactory.ERROR_NAME_NOT_REGISTERED), 
-		NAME_WITH_SPACE("For security purposes sending payments to a name that starts or ends with spaces is forbidden.", 
+	public enum NameResult {
+		OK("OK", "OK", 0), NAME_NOT_REGISTERED("The name is not registered",
+				"Invalid address or name not registered!",
+				ApiErrorFactory.ERROR_NAME_NOT_REGISTERED), NAME_WITH_SPACE(
+				"For security purposes sending payments to a name that starts or ends with spaces is forbidden.",
 				"Name Payments with trailing or leading spaces are not allowed!",
-				ApiErrorFactory.ERROR_NAME_WITH_SPACE), 
-		NAME_FOR_SALE("For security purposes sending payments to a name that can be purchased through name exchange is disabled.", 
+				ApiErrorFactory.ERROR_NAME_WITH_SPACE), NAME_FOR_SALE(
+				"For security purposes sending payments to a name that can be purchased through name exchange is disabled.",
 				"Payments with names that are for sale are not allowed!",
 				ApiErrorFactory.ERROR_NAME_FOR_SALE);
-		
+
 		private String statusMessage;
 		private String shortStatusMessage;
 		private int errorcode;
 
-		private NameResult(String statusMessage, String shortStatusMessage, int errorcode)
-		{
+		private NameResult(String statusMessage, String shortStatusMessage,
+				int errorcode) {
 			this.statusMessage = statusMessage;
 			this.shortStatusMessage = shortStatusMessage;
 			this.errorcode = errorcode;
-			
+
 		}
 
 		public String getStatusMessage() {
 			return statusMessage;
 		}
-		
+
 		public String getShortStatusMessage() {
 			return shortStatusMessage;
 		}
-		
+
 		public int getErrorCode() {
 			return errorcode;
 		}
 
 	}
-	
-	public static Pair<Account, NameResult> nameToAdress(String name)
-	{
+
+	public static Pair<Account, NameResult> nameToAdress(String name) {
 		NameMap names = DBSet.getInstance().getNameMap();
-		//NAME NOT REGISTERED?
-		if( !names.contains(name))
-		{
-			
-			return new Pair<Account, NameUtils.NameResult>(null, NameResult.NAME_NOT_REGISTERED);
-			
+		// NAME NOT REGISTERED?
+		if (!names.contains(name)) {
+
+			return new Pair<Account, NameUtils.NameResult>(null,
+					NameResult.NAME_NOT_REGISTERED);
+
 		}
-			
-		//NAME STARTS OR ENDS WITH SPACE?
-		if(name.startsWith(" ") || name.endsWith(" "))
-		{
-			return new Pair<Account, NameUtils.NameResult>(null, NameResult.NAME_WITH_SPACE);
+
+		// NAME STARTS OR ENDS WITH SPACE?
+		if (name.startsWith(" ") || name.endsWith(" ")) {
+			return new Pair<Account, NameUtils.NameResult>(null,
+					NameResult.NAME_WITH_SPACE);
 		}
-		
-		//NAME FOR SALE?
-		if(	DBSet.getInstance().getNameExchangeMap().contains(name))
-		{
-			return new Pair<Account, NameUtils.NameResult>(null, NameResult.NAME_FOR_SALE);
+
+		// NAME FOR SALE?
+		if (DBSet.getInstance().getNameExchangeMap().contains(name)) {
+			return new Pair<Account, NameUtils.NameResult>(null,
+					NameResult.NAME_FOR_SALE);
 		}
-		
-		
-		//LOOKUP ADDRESS FOR NAME
+
+		// LOOKUP ADDRESS FOR NAME
 		Name lookupName = names.get(name);
 		String recipientAddress = lookupName.getOwner().getAddress();
 		Account recipient = new Account(recipientAddress);
 		return new Pair<Account, NameUtils.NameResult>(recipient, NameResult.OK);
 	}
-	
-	
-	public static List<Pair<String, String>> getWebsitesByValue(String searchvalue)
-	{
+
+	public static List<Pair<String, String>> getWebsitesByValue(
+			String searchvalue) {
 		return getWebsitesbyValueInternal(searchvalue);
 
-		
-		
 	}
-	
-	
-	
-	
-	public static  List<Pair<String, String>> getNamesContainingWebsites() {
+
+	public static List<Pair<String, String>> getNamesContainingWebsites() {
 		return getWebsitesbyValueInternal(null);
 	}
-	
-	
+
 	@SuppressWarnings("unchecked")
-	public static JSONObject getJsonForNameOpt( Name name)
-	{
-		
+	public static JSONObject getJsonForNameOpt(Name name) {
+
 		String rawNameValue = null;
-		if(Controller.getInstance().doesWalletExists())
-		{
-			List<Transaction> accountTransactions = getOwnUnconfirmedTX();
-			
-			for (Transaction transaction : accountTransactions) {
-				
-				if(transaction.getType() == Transaction.UPDATE_NAME_TRANSACTION )
-				{
-					UpdateNameTransaction updateNameTx =	(UpdateNameTransaction) transaction;
-					if(updateNameTx.getName().getName().equals(name.getName()))
-					{
-						rawNameValue =  updateNameTx.getName().getValue();
-						break;
-					}
-					
+		List<Transaction> accountTransactions = getOwnUnconfirmedTX();
+
+		for (Transaction transaction : accountTransactions) {
+
+			if (transaction.getType() == Transaction.UPDATE_NAME_TRANSACTION) {
+				UpdateNameTransaction updateNameTx = (UpdateNameTransaction) transaction;
+				if (updateNameTx.getName().getName().equals(name.getName())) {
+					rawNameValue = updateNameTx.getName().getValue();
+					break;
 				}
+
 			}
-			
 		}
-		
-		
-		if(rawNameValue == null)
-		{
+
+		if (rawNameValue == null) {
 			rawNameValue = name.getValue();
 		}
-		
+
 		String decompressedNameValue = GZIP.webDecompress(rawNameValue);
-		
+
 		JSONObject jsonValue;
-		//THIS SIGNIFICANTLY INCREASES SPEED!
-		if(!decompressedNameValue.startsWith("{"))
-		{
+		// THIS SIGNIFICANTLY INCREASES SPEED!
+		if (!decompressedNameValue.startsWith("{")) {
 			jsonValue = new JSONObject();
 			jsonValue.put(Qorakeys.DEFAULT.toString(), decompressedNameValue);
 			return jsonValue;
 		}
-		
+
 		try {
 			jsonValue = (JSONObject) JSONValue.parse(decompressedNameValue);
-			
-			if(jsonValue == null)
-			{
+
+			if (jsonValue == null) {
 				jsonValue = new JSONObject();
-				jsonValue.put(Qorakeys.DEFAULT.toString(), decompressedNameValue);
+				jsonValue.put(Qorakeys.DEFAULT.toString(),
+						decompressedNameValue);
 			}
-			
+
 			return jsonValue;
-			
+
 		} catch (Exception e) {
-//			no valid json
-			
+			// no valid json
+
 			jsonValue = new JSONObject();
 			jsonValue.put(Qorakeys.DEFAULT.toString(), decompressedNameValue);
 			return jsonValue;
 		}
-		
-		
-		
+
 	}
 
-
 	public static List<Transaction> getOwnUnconfirmedTX() {
-		List<Transaction> transactions = DBSet.getInstance().getTransactionMap().getTransactions();
+		List<Transaction> transactions = DBSet.getInstance()
+				.getTransactionMap().getTransactions();
 		List<Transaction> accountTransactions = new ArrayList<Transaction>();
-		
-		for(Transaction transaction: transactions)
-		{
-			if(Controller.getInstance().getAccounts().contains(transaction.getCreator()))
-			{
+
+		for (Transaction transaction : transactions) {
+			if (Controller.getInstance().getAccounts()
+					.contains(transaction.getCreator())) {
 				accountTransactions.add(transaction);
 			}
 		}
-			
-		//SORT THEM BY TIMESTAMP
-		Collections.sort(accountTransactions, new TransactionTimestampComparator());
+
+		// SORT THEM BY TIMESTAMP
+		Collections.sort(accountTransactions,
+				new TransactionTimestampComparator());
 		Collections.reverse(accountTransactions);
 		return accountTransactions;
 	}
 
+	public static List<Pair<String, String>> getWebsitesbyValueInternal(
+			String searchValueOpt) {
 
-	public static List<Pair<String, String>> getWebsitesbyValueInternal( String searchValueOpt) {
-		
-		
-		List<Pair<String, String>> results = new ArrayList<Pair<String,String>>();
+		List<Pair<String, String>> results = new ArrayList<Pair<String, String>>();
 
 		NameMap nameMap = DBSet.getInstance().getNameMap();
 
@@ -207,39 +183,43 @@ public class NameUtils {
 			value = GZIP.webDecompress(value);
 			value = WebResource.injectValues(value);
 
-				try {
-					if(value.startsWith("{"))
-					{
-						JSONObject jsonObject = (JSONObject) JSONValue.parse(value);
-						if(jsonObject.containsKey(Qorakeys.WEBSITE.getKeyname()))
-						{
-							String websitevalue = (String) jsonObject.get(Qorakeys.WEBSITE.getKeyname());
-							if(websitevalue.contains(Qorakeys.WEBSITE.getKeyname()) && websitevalue.matches("<inj.*key=."+Qorakeys.WEBSITE.getKeyname()+ ".*>"))
-							{
-//								Another website is injected into this page, to avoid both in the searchengine we don't allow this!
-								continue;
+			try {
+				if (value.startsWith("{")) {
+					JSONObject jsonObject = (JSONObject) JSONValue.parse(value);
+					if (jsonObject.containsKey(Qorakeys.WEBSITE.getKeyname())) {
+						String websitevalue = (String) jsonObject
+								.get(Qorakeys.WEBSITE.getKeyname());
+						if (websitevalue
+								.contains(Qorakeys.WEBSITE.getKeyname())
+								&& websitevalue
+										.matches("<inj.*key=."
+												+ Qorakeys.WEBSITE.getKeyname()
+												+ ".*>")) {
+							// Another website is injected into this page, to
+							// avoid both in the searchengine we don't allow
+							// this!
+							continue;
+						}
+						websitevalue = WebResource.injectValues(websitevalue);
+						if (searchValueOpt == null) {
+							results.add(new Pair<String, String>(key,
+									websitevalue));
+						} else {
+							if (websitevalue.toLowerCase().contains(
+									searchValueOpt.toLowerCase())) {
+								results.add(new Pair<String, String>(key,
+										websitevalue));
 							}
-							websitevalue =	WebResource.injectValues(websitevalue);
-							if(searchValueOpt == null)
-							{
-								results.add(new Pair<String,String>(key,websitevalue));
-							}else
-							{
-								if(websitevalue.toLowerCase().contains(searchValueOpt.toLowerCase()))
-								{
-									results.add(new Pair<String,String>(key,websitevalue));
-								}
-								
-							}
+
 						}
 					}
-				} catch (Exception e) {
-					// no valid json no website key
 				}
+			} catch (Exception e) {
+				// no valid json no website key
+			}
 
 		}
 
-		
 		return results;
 	}
 }
