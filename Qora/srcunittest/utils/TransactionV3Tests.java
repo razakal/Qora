8693e608d315f7a2a22f83fc5572ec7077c45aac
agran <agran@agran.net>
2016-02-04 08:15:32 +0300

    Added test for ArbitraryTransactionV3 without Payments

--- Qora/srcunittest/utils/TransactionV3Tests.java
+++ Qora/srcunittest/utils/TransactionV3Tests.java
@@ -212,4 +212,88 @@ public class TransactionV3Tests {
 		assertEquals(arbitraryTransactionV3.isSignatureValid(), true);
 		assertEquals(arbitraryTransactionV3_2.isSignatureValid(), true);		
 	}	
+	
+	@Test
+	public void validateArbitraryTransactionV3withoutPayments() 
+	{
+		
+		//CREATE EMPTY MEMORY DATABASE
+		DBSet databaseSet = DBSet.createEmptyDatabaseSet();
+		
+		//ADD QORA ASSET
+		Asset qoraAsset = new Asset(new GenesisBlock().getGenerator(), "Qora", "This is the simulated Qora asset.", 10000000000L, true, new byte[64]);
+		Asset aTFundingAsset = new Asset(new GenesisBlock().getGenerator(), "ATFunding", "This asset represents the funding of AT team for the integration of a Turing complete virtual machine into Qora.", 250000000L, true, new byte[64]);
+		databaseSet.getAssetMap().set(0l, qoraAsset);
+		databaseSet.getAssetMap().set(61l, aTFundingAsset);
+    	
+		GenesisBlock genesisBlock = new GenesisBlock();
+		genesisBlock.process(databaseSet);
+		
+		//CREATE KNOWN ACCOUNT
+		byte[] seed = Crypto.getInstance().digest("test".getBytes());
+		byte[] privateKey = Crypto.getInstance().createKeyPair(seed).getA();
+		
+		byte[] data = "test123!".getBytes();
+		
+		PrivateKeyAccount creator = new PrivateKeyAccount(privateKey);
+		
+		long timestamp = NTP.getTime();
+
+		//PROCESS GENESIS TRANSACTION TO MAKE SURE SENDER HAS FUNDS
+		Transaction transaction = new GenesisTransaction(creator, BigDecimal.valueOf(1000).setScale(8), NTP.getTime());
+		transaction.process(databaseSet);
+		
+		creator.setConfirmedBalance(61l, BigDecimal.valueOf(1000).setScale(8), databaseSet);
+		
+		List<Payment> payments = new ArrayList<Payment>();
+		
+		byte[] signature = ArbitraryTransactionV3.generateSignature(
+				databaseSet, 
+				creator, payments, 111, data,
+				BigDecimal.valueOf(1).setScale(8), 
+				timestamp
+				);
+		
+		ArbitraryTransactionV3 arbitraryTransactionV3 = new ArbitraryTransactionV3(
+				creator, payments, 111, data,
+				BigDecimal.valueOf(1).setScale(8), 
+				timestamp,
+				creator.getLastReference(databaseSet),
+				signature
+				);
+		
+		if (NTP.getTime() < Transaction.ARBITRARY_TRANSACTIONS_RELEASE || arbitraryTransactionV3.getTimestamp() < Block.POWFIX_RELEASE)
+		{
+			assertEquals(arbitraryTransactionV3.isValid(databaseSet), Transaction.NOT_YET_RELEASED);
+		}
+		else
+		{
+			assertEquals(arbitraryTransactionV3.isValid(databaseSet), Transaction.VALIDATE_OKE);
+		}
+		
+		arbitraryTransactionV3.process(databaseSet);
+		
+		assertEquals(BigDecimal.valueOf(999).setScale(8), creator.getConfirmedBalance(databaseSet));
+		assertEquals(BigDecimal.valueOf(1000).setScale(8), creator.getConfirmedBalance(61l, databaseSet));
+
+		
+		byte[] rawArbitraryTransactionV3 = arbitraryTransactionV3.toBytes();
+		
+		ArbitraryTransactionV3 arbitraryTransactionV3_2 = null;
+		try {
+			arbitraryTransactionV3_2 = (ArbitraryTransactionV3) ArbitraryTransactionV3.Parse(Arrays.copyOfRange(rawArbitraryTransactionV3, 4, rawArbitraryTransactionV3.length));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertEquals(new String(arbitraryTransactionV3.getData()), new String(arbitraryTransactionV3_2.getData()));
+
+		assertEquals( 	arbitraryTransactionV3.getPayments().size(), arbitraryTransactionV3.getPayments().size());  
+
+		assertEquals(arbitraryTransactionV3.getService(), arbitraryTransactionV3_2.getService());
+		assertEquals(arbitraryTransactionV3.getCreator(), arbitraryTransactionV3_2.getCreator());
+
+		assertEquals(arbitraryTransactionV3.isSignatureValid(), true);
+		assertEquals(arbitraryTransactionV3_2.isSignatureValid(), true);		
+	}	
+	
 }
