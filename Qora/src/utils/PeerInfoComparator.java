dcdd13eff9cfb9834e1fc3637bdfe588af3d8ca2
agran <agran@agran.net>
2016-02-07 13:09:28 +0300

    fix in PeerInfoComparator

--- Qora/src/utils/PeerInfoComparator.java
+++ Qora/src/utils/PeerInfoComparator.java
@@ -15,8 +15,8 @@ public class PeerInfoComparator implements Comparator<PeerInfo> {
 		//boolean o1WhiteOldDayAgo = (now - o1.getFindTime() > 1*24*60*60*1000);  
 		//boolean o2WhiteOldDayAgo = (now - o2.getFindTime() > 1*24*60*60*1000);  
 		
-		boolean o1findMoreWeekAgo = (now - o1.getFindTime() > 7*24*60*60*1000);  
-		boolean o2findMoreWeekAgo = (now - o2.getFindTime() > 7*24*60*60*1000);  
+		boolean o1findMoreWeekAgo = (now - o1.getFindingTime() > 7*24*60*60*1000);  
+		boolean o2findMoreWeekAgo = (now - o2.getFindingTime() > 7*24*60*60*1000);  
 		
 		boolean o1neverWhite = o1.getWhitePingCouner() == 0;
 		boolean o2neverWhite = o2.getWhitePingCouner() == 0;
