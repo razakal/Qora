bceb7719daf64e169216d05c0da2ac5bb98774e2
agran <agran@agran.net>
2016-02-07 14:05:36 +0300

    Improvement of the table on peers in debug window

--- Qora/src/gui/models/PeersTableModel.java
+++ Qora/src/gui/models/PeersTableModel.java
@@ -2,23 +2,49 @@ package gui.models;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.table.AbstractTableModel;
 
-import utils.ObserverMessage;
 import controller.Controller;
+import database.DBSet;
+import database.PeerMap.PeerInfo;
 import network.Peer;
+import settings.Settings;
+import utils.DateTimeFormat;
+import utils.ObserverMessage;
 
 @SuppressWarnings("serial")
 public class PeersTableModel extends AbstractTableModel implements Observer{
 
+	private static final int COLUMN_ADDRESS = 0;
+	private static final int COLUMN_HEIGHT = 1;
+	private static final int COLUMN_PINGMC = 2;
+	private static final int COLUMN_REILABLE = 3;
+	private static final int COLUMN_INITIATOR = 4;
+	private static final int COLUMN_FINDING_AGO = 5;
+	
+	private Timer timer = new Timer();
+	
 	private List<Peer> peers;
 	
-	private String[] columnNames = {"IP"};
+	private String[] columnNames = {"IP", "Height", "Ping mc", "Reliable", "Initiator", "Finding ago"};
 	
 	public PeersTableModel()
 	{
 		Controller.getInstance().addActivePeersObserver(this);
+		
+		this.timer.cancel();
+		this.timer = new Timer();
+		
+		TimerTask action = new TimerTask() {
+	        public void run() {
+	        		fireTableDataChanged();	        	
+	        }
+		};
+		
+		this.timer.schedule(action, Settings.getInstance().getPingInterval());
 	}
 	
 	@Override
@@ -47,12 +73,48 @@ public class PeersTableModel extends AbstractTableModel implements Observer{
 	@Override
 	public Object getValueAt(int row, int column)
 	{
-		if(peers == null)
+		if(peers == null || this.peers.size() -1 < row)
 		{
 			return null;
 		}
 		
-		return peers.get(row).getAddress().getHostAddress();
+		Peer peer = peers.get(row);
+		
+		if(DBSet.getInstance().isStoped())
+			return null;
+			
+		PeerInfo peerInfo = DBSet.getInstance().getPeerMap().getInfo(peer.getAddress());
+		switch(column)
+		{
+			case COLUMN_ADDRESS:
+				return peer.getAddress().getHostAddress();
+
+			case COLUMN_HEIGHT:
+				return Controller.getInstance().getPeerHeights().get(peer);
+			
+			case COLUMN_PINGMC:
+				if(peer.getPing() > 1000000) {
+					return "Waiting...";
+				} else {
+					return peer.getPing();
+				}
+			
+			case COLUMN_REILABLE:
+				return peerInfo.getWhitePingCouner();
+			
+			case COLUMN_INITIATOR:
+				if(peer.isWhite()) {
+					return "You";
+				} else {
+					return "Remote";
+				}
+			
+			case COLUMN_FINDING_AGO:
+				return DateTimeFormat.timeAgo(peerInfo.getFindingTime());
+				
+		}
+		
+		return null;
 	}
 
 	@Override
