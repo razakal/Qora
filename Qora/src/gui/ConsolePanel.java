818079876b0663bc4ca2aaca9821bc8babd82fcb
agran <agran@agran.net>
2016-02-13 22:54:26 +0300

    Added button Run to Gui Console

--- Qora/src/gui/ConsolePanel.java
+++ Qora/src/gui/ConsolePanel.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -7,10 +9,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import java.util.ArrayList;
-import utils.MenuPopupUtil;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
@@ -19,6 +20,7 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import api.ApiClient;
+import utils.MenuPopupUtil;
 
 @SuppressWarnings("serial")
 public class ConsolePanel extends JPanel 
@@ -39,25 +41,20 @@ public class ConsolePanel extends JPanel
 		//PADDING
 		this.setBorder(new EmptyBorder(10, 10, 10, 10));
 		
+		int componentLevel = 0;
+		
 		//TEXTAREA GBC
 		GridBagConstraints areaGBC = new GridBagConstraints();
 		areaGBC.insets = new Insets(5,5,5,5);
 		areaGBC.fill = GridBagConstraints.BOTH;  
 		areaGBC.anchor = GridBagConstraints.NORTHWEST;
+		
+		areaGBC.gridy = componentLevel;
+		areaGBC.gridx = 0;	
 		areaGBC.weighty = 1;	
 		areaGBC.weightx = 1;
-		areaGBC.gridx = 0;	
-		areaGBC.gridy = 0;
-		
-		//TEXTBOX GBC
-		GridBagConstraints txtGBC = new GridBagConstraints();
-		txtGBC.insets = new Insets(5,5,5,5);
-		txtGBC.fill = GridBagConstraints.HORIZONTAL;  
-		txtGBC.anchor = GridBagConstraints.NORTHWEST;
-		txtGBC.weightx = 1;
-		txtGBC.gridx = 0;	
-		txtGBC.gridy = 1;
-		
+		areaGBC.gridwidth = 2;
+
 		//TEXTAREA
 		this.areaConsole = new JTextArea();
 		this.areaConsole.setLineWrap(true);
@@ -73,31 +70,7 @@ public class ConsolePanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent e)
             {
-				//GET COMMAND
-				String command = txtCommand.getText();
-				
-				cmdHistory.add(command);
-				INTcmdHistory = cmdHistory.size();
-				
-				if(command.toLowerCase().equals("clear"))
-				{
-					areaConsole.setText("");
-					txtCommand.setText("");
-					return;
-				}
-					
-				areaConsole.append("[COMMAND] " + command + "\n");
-				
-				//EMPTY COMMAND FIELD
-				txtCommand.setText("");
-				
-				//GET RESULT
-				String result = client.executeCommand(command);
-				
-				//APPEND RESULT
-				areaConsole.append("[RESULT] " + result + "\n");
-				areaConsole.setCaretPosition(areaConsole.getDocument().getLength());
-
+				execute();
             }
 		});
 		
@@ -125,7 +98,40 @@ public class ConsolePanel extends JPanel
 		    } 
 		});
 		
+		componentLevel ++;
+		
+		//TEXTBOX GBC
+		GridBagConstraints txtGBC = new GridBagConstraints();
+		txtGBC.insets = new Insets(5,5,5,5);
+		txtGBC.fill = GridBagConstraints.HORIZONTAL;  
+		txtGBC.anchor = GridBagConstraints.CENTER;
+		
+		txtGBC.gridy = componentLevel;
+		txtGBC.gridwidth = 1;
+		txtGBC.gridx = 0;	
+		txtGBC.weightx = 1;
+		
 		this.add(this.txtCommand, txtGBC);
+		
+        //BUTTON RUN
+		GridBagConstraints buttonGBC = new GridBagConstraints();
+		
+		buttonGBC.gridy = componentLevel;
+		buttonGBC.gridx = 1;
+		buttonGBC.gridwidth = 1;
+		
+		buttonGBC.fill = GridBagConstraints.HORIZONTAL;  
+		buttonGBC.anchor = GridBagConstraints.CENTER;
+		JButton sendButton = new JButton("Run");
+        sendButton.setPreferredSize(new Dimension(80, 25));
+    	sendButton.addActionListener(new ActionListener()
+		{
+		    public void actionPerformed(ActionEvent e)
+		    {
+		    	execute();
+		    }
+		});	
+		this.add(sendButton, buttonGBC);
 		
 		//CONTEXT MENU
 		MenuPopupUtil.installContextMenu(this.areaConsole);
@@ -138,5 +144,33 @@ public class ConsolePanel extends JPanel
 					txtCommand.requestFocus();
 				}
 			});
+	}
+	
+	public void execute()
+	{
+		//GET COMMAND
+		String command = txtCommand.getText();
+		
+		cmdHistory.add(command);
+		INTcmdHistory = cmdHistory.size();
+		
+		if(command.toLowerCase().equals("clear"))
+		{
+			areaConsole.setText("");
+			txtCommand.setText("");
+			return;
+		}
+			
+		areaConsole.append("[COMMAND] " + command + "\n");
+		
+		//EMPTY COMMAND FIELD
+		txtCommand.setText("");
+		
+		//GET RESULT
+		String result = client.executeCommand(command);
+		
+		//APPEND RESULT
+		areaConsole.append("[RESULT] " + result + "\n");
+		areaConsole.setCaretPosition(areaConsole.getDocument().getLength());
 	}
 }
