d839514daa7942a2d9a332bae9fdb4a5f8e73357
Skerberus <Buddelb@googlemail.com>
2016-03-24 20:30:59 +0100

    created log4j.default file which is used if log4j.properties is not found!

--- Qora/src/Start.java
+++ Qora/src/Start.java
@@ -1,27 +1,42 @@
+import gui.Gui;
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
+import lang.Lang;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
-import api.ApiClient;
-import controller.Controller;
-import gui.Gui;
-import lang.Lang;
 import settings.Settings;
 import utils.SysTray;
+import api.ApiClient;
+import controller.Controller;
 
 public class Start {
 	
 	static Logger LOGGER = Logger.getLogger(Start.class.getName());
 
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{	
 		
-		PropertyConfigurator.configure(new File( "log4j.properties").getAbsolutePath());
+		File log4j = new File("log4j.properties");
+		if(log4j.exists())
+		{
+			PropertyConfigurator.configure(log4j.getAbsolutePath());
+		}else
+		{
+			try( InputStream resourceAsStream = ClassLoader.class.getResourceAsStream("/log4j/log4j.default");)
+			{
+				PropertyConfigurator.configure(resourceAsStream);
+				LOGGER.error("log4j.properties not found, search path is " + log4j.getAbsolutePath() + " using default!");
+			}
+		}
 		
 		boolean cli = false;
 		
