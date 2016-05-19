4ac9f8615bbfc8105deab006f3e0e3c352be7f6d
agran <agran@agran.net>
2016-05-19 18:11:16 +0300

    fixed version to 0.26.0

--- Qora/src/controller/Controller.java
+++ Qora/src/controller/Controller.java
@@ -93,8 +93,8 @@ public class Controller extends Observable {
 	
 	
 	private static final Logger LOGGER = Logger.getLogger(Controller.class);
-	private String version = "0.26.0 beta";
-	private String buildTime = "2016-03-24 00:00:00 UTC";
+	private String version = "0.26.0";
+	private String buildTime = "2016-05-20 00:00:00 UTC";
 	private long buildTimestamp;
 	
 	public static final String releaseVersion = "0.26.0";
